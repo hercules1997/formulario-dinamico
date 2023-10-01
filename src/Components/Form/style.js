e9d8432c20fa7e8cda5099f0d15d6c7e9a2edcf9
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-10-01 09:00:43 -0300

    Add variáveis de cores

--- src/Components/Form/style.js
+++ src/Components/Form/style.js
@@ -1,19 +1,44 @@
 //Importação do estilos de componentes
+
 import styled from "styled-components";
+
+// Criando variáveis de cores 
+const colorsRoots = {
+  colorPrymary: "#663399",
+  colorSecondary: "#7D7E7E",
+  colorWhite: "#ffff",
+};
+
 // Estilo do formulário
 const FormStyle = styled.form`
   gap: 15px;
   display: flex;
   flex-direction: column;
-width: 80%;
+  width: 80%;
 
+  h2 {
+    border-bottom: 1px solid ${colorsRoots.colorSecondary};
+    margin-bottom: 25px;
+    padding-bottom: 9px;
+    color: ${colorsRoots.colorSecondary};
+  }
   button {
     width: 100%;
+  }
+
+  i {
+    color: ${colorsRoots.colorPrymary};
   }
 
   button[value="Cadastrar"] {
     margin-left: 35px;
     width: auto;
+    color: ${colorsRoots.colorPrymary};
+    border: 1px solid ${colorsRoots.colorPrymary};
+    &:hover {
+      background: ${colorsRoots.colorPrymary};
+      color: ${colorsRoots.colorWhite};
+    }
   }
   .container-input {
     display: flex;
@@ -24,18 +49,30 @@ width: 80%;
   // Eslilod dos inputs
   .input {
     outline: none;
-    color: #ffff !important;
+    color: ${colorsRoots.colorSecondary} !important;
     background: transparent;
     border: none;
-    border-bottom: 1px solid gray;
+    border-bottom: 1px solid ${colorsRoots.colorSecondary};
+
+    ::-webkit-calendar-picker-indicator {
+      filter: invert(1); /* Inverte a cor do ícone do calendário */
+      color: gray;
+    }
+
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+    }
     // Estilo do placeholder
     &::-webkit-input-placeholder {
-      color: gray !important;
+      color: ${colorsRoots.colorSecondary} !important;
     }
 
     // Eltilos do focus
     &:focus {
-      border: solid rebeccapurple 1px;
+      box-shadow: none;
+      box-shadow: bottom 0 0 50px ${colorsRoots.colorWhite} !important;
+      border: none !important;
+      border-bottom: 2px solid ${colorsRoots.colorPrymary} !important;
     }
   }
 
